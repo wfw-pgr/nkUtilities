0e6d407aa7f5dd9de7fa134a7e94f2b12b25810a
wfw-pgr <wfw.pgr@gmail.com>
2024-06-30 11:26:44 +0900

    update generate__html.py

--- generate__html.py
+++ generate__html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os, sys, json5, argparse, shutil
 import nkUtilities.json__formulaParser as jso
-
+import pandas as pd
 # ========================================================= #
 # ===  translate__json2html                             === #
 # ========================================================= #
@@ -12,17 +12,17 @@ def translate__json2html( data ):
     # --- [1] recursive translation                 --- #
     # ------------------------------------------------- #
     if   isinstance( data, type(None) ):           #  -- None -- #
-        return( '<div class="bool-true">None</div>' )
+        return( '<div class="bool-true">None</div>\n' )
     
     elif isinstance( data, bool ):                 #  -- bool -- #
         sval = str(data)
-        return( '<div class="bool-{0}">{1}</div>'.format( sval.lower(), sval ) )
+        return( '<div class="bool-{0}">{1}</div>\n'.format( sval.lower(), sval ) )
         
     elif isinstance( data, str  ):                 #  -- str  -- #
         if ( len(data) == 0):
-            return( '<span class="string empty">(Empty Text)</span>' )
+            return( '<span class="string empty">(Empty Text)</span>\n' )
         else:
-            return( f'<span class="string">{data}</span>' )
+            return( f'<span class="string">{data}</span>\n' )
         
     elif isinstance(data, (int, float)):
         # INT or FLOAT
@@ -30,44 +30,44 @@ def translate__json2html( data ):
             class_name = "int number"
         else:
             class_name = "float number"
-        return f'<span class="{class_name}">{data}</span>'
+        return f'<span class="{class_name}">{data}</span>\n'
     elif isinstance(data, list):
         # ARRAY
         if len(data) > 0:
-            html = '<table class="array">'
+            html = '<table class="array">\n'
             for i, item in enumerate(data):
-                html += '<tr>'
-                html += '<td class="value array-value">'
+                html += '<tr>\n'
+                html += '<td class="value array-value">\n'
                 html += translate__json2html( data=item )
-                html += '</td></tr>'
-            html += '</table>'
+                html += '</td></tr>\n'
+            html += '</table>\n'
             return html
         else:
-            return '<span class="array empty">(Empty List)</span>'
+            return '<span class="array empty">(Empty List)</span>\n'
     elif isinstance( data, dict ):
         # OBJECT
-        html = '<table class="object">'
+        html = '<table class="object">\n'
         for key, value in data.items():
-            html += '<tr>'
-            html += f'<th class="key object-key">{key}</th>'
-            html += '<td class="value object-value">'
+            html += '<tr>\n'
+            html += f'<th class="key object-key">{key}</th>\n'
+            html += '<td class="value object-value">\n'
             html += translate__json2html( data=value )
-            html += '</td></tr>'
-        html += '</table>'
+            html += '</td></tr>\n'
+        html += '</table>\n'
         return html
     elif callable(data):
         # FUNCTION
-        return f'<span class="function">{data}</span>'
+        return f'<span class="function">{data}</span>\n'
     else:
         # UNKNOWN
-        return f'<span class="unknown">{data}</span>'
+        return f'<span class="unknown">{data}</span>\n'
 
 
 # ========================================================= #
 # ===  generate__html                                   === #
 # ========================================================= #
 
-def generate__html( html_lines=[],  html_config=None ):
+def generate__html( html_lines=[],  html_config=None, silent=False ):
 
     
     if ( html_config is None ): sys.exit( "[generate__html.py] html_config == ???" )
@@ -79,7 +79,8 @@ def generate__html( html_lines=[],  html_config=None ):
     if ( "title"      in settings ): title      = settings[ "title"      ]
     if ( "paramsFile" in settings ): paramsFile = settings[ "paramsFile" ]
     if ( "htmlFile"   in settings ): htmlFile   = settings[ "htmlFile"   ]
-    
+    if ( "csvFile"    in settings ): csvFile    = settings[ "csvFile"    ]
+
     # ------------------------------------------------- #
     # --- [1] html header part                      --- #
     # ------------------------------------------------- #
@@ -92,14 +93,14 @@ def generate__html( html_lines=[],  html_config=None ):
             out_dirpath  = os.path.dirname ( htmlFile )
             css_copypath = os.path.join( out_dirpath, css_filepath )
             shutil.copy( cssFile, css_copypath )
-        html_lines  += [ f'<link rel="stylesheet" type="text/css" href="{css_filepath}"/>' ] 
+        html_lines  += [ f'<link rel="stylesheet" type="text/css" href="{css_filepath}"/>\n' ] 
     html_lines      += [ "</head>", "<body>" ]
    
     # ------------------------------------------------- #
     # --- [2] html body part                        --- #
     # ------------------------------------------------- #
     if ( title ):
-        html_lines += [ f'<h1>{title}</h1>' ]
+        html_lines += [ f'<h1>{title}</h1>\n' ]
     if ( paramsFile ):
         if ( os.path.exists( paramsFile ) ):
             data  = jso.json__formulaParser( inpFile=paramsFile )
@@ -112,12 +113,22 @@ def generate__html( html_lines=[],  html_config=None ):
     # ------------------------------------------------- #
     # --- [3] html image file                       --- #
     # ------------------------------------------------- #
+    if ( csvFile ):
+        csvData     = pd.read_csv( csvFile )
+        html_lines += [ csvData.to_html( classes='table_design' ) ]
+
+    # ------------------------------------------------- #
+    # --- [4] html image file                       --- #
+    # ------------------------------------------------- #
     imgtag = '<img src="../{0}" width="{1}">'
     for key,obj in images.items():
-        html_lines += [ imgtag.format( obj["filepath"], obj["width"] ) ]
+        command     = imgtag.format( obj["filepath"], obj["width"] )
+        if ( not( "line_break" in obj  ) ): obj["line_break"] = True
+        if ( obj["line_break"] is True   ): command = f"<p> {command} </p>"
+        html_lines += [ command ]
         
     # ------------------------------------------------- #
-    # --- [3] html closing part / save html file    --- #
+    # --- [5] html closing part / save html file    --- #
     # ------------------------------------------------- #
     html_lines  += [ "</body>", "</html>" ]
     html_strings = "\n".join( html_lines )
@@ -125,6 +136,8 @@ def generate__html( html_lines=[],  html_config=None ):
     if ( htmlFile ):
         with open( htmlFile, "w" ) as f:
             f.write( html_strings )
+        if not( silent ):
+            print( "[generate__html.py] output file :: {}".format( htmlFile ) )
     return( html_strings )
             
     
@@ -142,6 +155,8 @@ if ( __name__=="__main__" ):
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument( "--html_config", help="system config file for html." )
+    parser.add_argument( "-s","--silent", help="no display", \
+                         default=False, action="store_true" )
     args   = parser.parse_args()
 
     if args.html_config:
@@ -152,5 +167,5 @@ if ( __name__=="__main__" ):
     # ------------------------------------------------- #
     # --- [2] call arguments                        --- #
     # ------------------------------------------------- #
-    generate__html( html_config=html_config )
+    generate__html( html_config=html_config, silent=args.silent )
     
